78c27d802fca1f00a44f296b9a0d0c716588a17c
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2020-07-23 13:44:31 -0300

    Adequa id do recurso

--- scripts/publish.R
+++ scripts/publish.R
@@ -8,7 +8,7 @@ source_python("scripts/lib/utils.py")
 output <- jsonlite::read_json("datapackage.json")$resources[[1]]$path
 
 res <- upload_resource(output, 
-                       resource_id = "42fd644d-6a50-4539-a825-05b2460adfa0", 
+                       resource_id = "43570463-76a2-407e-89f9-4bfd10638354", 
                        url = "http://dados.mg.gov.br", 
                        key = Sys.getenv("DADOSMG_PROD"))
 
