dd7d5b00fe082232576377296fb2229ccea2a969
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2020-08-06 17:53:11 -0300

    Remove dependencia python para upload CKAN

--- scripts/publish.R
+++ scripts/publish.R
@@ -1,14 +1,9 @@
-library(reticulate)
-
 source("scripts/lib/utils.R")
-
-use_virtualenv("r-reticulate", required = TRUE)
-source_python("scripts/lib/utils.py")
 
 output <- jsonlite::read_json("datapackage.json")$resources[[1]]$path
 
-res <- upload_resource(output, 
-                       resource_id = "43570463-76a2-407e-89f9-4bfd10638354", 
+res <- ckanr::resource_update(id = "43570463-76a2-407e-89f9-4bfd10638354",
+                       path = output, 
                        url = "http://dados.mg.gov.br", 
                        key = Sys.getenv("DADOSMG_PROD"))
 
