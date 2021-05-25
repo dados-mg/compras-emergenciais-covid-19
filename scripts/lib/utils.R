f606b2fba6b3ae219535c8714dbfcd1e16ef89fb
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2021-05-25 16:22:24 -0300

    [#7] Adiciona definicao para funcao check_upload

--- scripts/lib/utils.R
+++ scripts/lib/utils.R
@@ -137,3 +137,14 @@ lookup_link_registro_preco <- function(x) {
   
   lookup[x] %>% unname()
 }
+
+check_upload <- function(file, url) {
+  file_hash <- digest::digest(file, file = TRUE)
+  
+  tmp <- tempfile()
+  download.file(url, tmp)
+  url_hash <- digest::digest(tmp, file = TRUE)
+  unlink(tmp)  
+  
+  file_hash == url_hash
+}
